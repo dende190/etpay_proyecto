a89c752924ffe76e40beee31e196327363eda08c
Juan Pablo Arnedo <juan.arnedo@zalvadora.com>
2022-11-02 22:40:48 -0500

    crear queryTransaction

---
+++ etpay-serverless/src/queryTransaction.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const { queryBuild } = require('../utils/query_build');
+const TABLE_NAME = process.env.TABLE_NAME;
+
+module.exports.queryTransaction = async (event) => {
+  const { select, where } = JSON.parse(event.body);
+
+  const dynamoDB = new AWS.DynamoDB.DocumentClient();
+  const condition = queryBuild.generateCondition(where);
+  const params = {
+      TableName: TABLE_NAME,
+      ProjectionExpression: select,
+      FilterExpression: condition.updateExpressions.join(' AND '),
+      ExpressionAttributeValues: condition.expressionAttributeValues,
+    };
+  const transactions = await (
+    dynamoDB
+    .scan(params)
+    .promise()
+  );
+
+  return {
+    status: 200,
+    body: transactions.Items,
+  };
+};
